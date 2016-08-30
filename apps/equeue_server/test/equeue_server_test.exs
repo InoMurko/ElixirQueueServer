24a0ee4786b1d91ef544375dd11d111ac1d14e18
Ino Murko <ino.murko@outlook.com>
2016-08-30 22:58:20 +0100

    db additions fifo test

---
+++ apps/equeue_server/test/equeue_server_test.exs
@@ -0,0 +1,41 @@
+defmodule EqueueServerTest do
+  use ExUnit.Case
+  doctest EqueueServer
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+  test "fifo", context do
+    :ok = QlibApi.add("OneAndOnly", "Message1")
+    :ok = QlibApi.destroy("OneAndOnly")
+    :ok = QlibApi.add("OneAndOnly", "Message")
+    :ok = QlibApi.add("JustOne", "Message")
+    :ok = QlibApi.add("owner1", "Message")
+    :ok = QlibApi.add("owner2", "TestMessage")
+    :ok = QlibApi.add("owner2", "TestMessage22221")
+    :ok = QlibApi.add("owner1", "Message")
+    :ok = QlibApi.add("owner2", "TestMessage22222")
+    :ok = QlibApi.add("owner1", "Message")
+    :ok = QlibApi.add("OneAndOnly", "Message2")
+    :ok = QlibApi.add("owner2", "TestMessage22223")
+    assert "Message" == QlibApi.get("owner1")
+    assert "TestMessage" == QlibApi.get("owner2")
+    assert "TestMessage22221" == QlibApi.get("owner2")
+    assert "TestMessage22222" == QlibApi.get("owner2")
+    assert "TestMessage22223" == QlibApi.get("owner2")
+    assert "Message" == QlibApi.get("owner1")
+    :ok = QlibApi.add("owner2", "TestMessage")
+    assert "Message" == QlibApi.get("owner1")
+    assert "TestMessage" == QlibApi.get("owner2")
+    assert '$end_of_table' == QlibApi.get("owner1")
+    assert '$end_of_table' == QlibApi.get("owner2")
+    :ok = QlibApi.destroy("OneAndOnly")
+    :ok = QlibApi.destroy("JustOne")
+    :ok = QlibApi.destroy("JustNothing")
+    assert '$end_of_table' == QlibApi.get("OneAndOnly")
+    assert '$end_of_table' == QlibApi.get("JustOne")
+    assert [] == :ets.tab2list(:qlib_queue)
+  end
+						  
+end
--- apps/qlib/lib/qlib_db.ex
+++ apps/qlib/lib/qlib_db.ex
@@ -34,7 +34,7 @@ defmodule QlibDB do
   the queue! This would return the first insert, regardles of the queue name.                      
   """
   @spec first(:mnesia.tab()) :: C.end_of_table | C.qlib_request
-  defp first(table) do
+  def first(table) do
     case :mnesia.dirty_first(table) do
       C.end_of_table ->
 	C.end_of_table
@@ -43,13 +43,53 @@ defmodule QlibDB do
         qRec
     end
   end
+
+  @doc """
+  There's the strong assumption that only one and the same process
+  is using this function, and that writes only happen at the end of 
+  the queue! This would return the first insert that matches the queue name.
+  """
+  @spec first_select(:mnesia.tab(), String.t()) :: C.end_of_table | binary()
+  def first_select(table, queue) do
+    fun = fn -> :mnesia.select(table,[{{'$1','$2','$3','$4'},
+				       [{'=:=','$4',{:const, queue}}],
+				       [{{'$2','$3'}}]}], 1, :read) end
+    case :mnesia.transaction(fun) do
+      {:atomic, C.end_of_table} ->
+        C.end_of_table
+      {:atomic, {[qRec],_}} -> 
+        #return just message
+	{key, message} = qRec
+        delete_chk_q(table, key)
+        message
+    end
+  end
+
+  # Deletes an entry.
+  @spec delete_chk_q(:mnesia.tab(), {:erlang.timestamp(), String.t()}) :: :ok | no_return()
+  defp delete_chk_q(table, key) do
+    :mnesia.dirty_delete({table, key})
+  end
+  
+  @doc """
+  Checks whether the key provided is the first, then deletes it.
+  """
+  @spec delete_chk(:mnesia.tab(), {:erlang.timestamp(), String.t()}) :: :ok | no_return()
+  def delete_chk(table, key) do
+    case :mnesia.dirty_first(table) do
+      ^key ->
+	:mnesia.dirty_delete(table, key)
+      first ->
+	raise({:bad_delete, table, key, first})
+    end
+  end
   
   @doc """
   Creates and adds an entry to one of the Mnesia table. It is added
   at the end of the ordered_set.
   """
   @spec add(:mnesia.tab(), String.t(), String.t(), binary()) :: :ok
-  defp add(table, id, queue, msg) do
+  def add(table, id, queue, msg) do
     key = {:os.timestamp(), id}
     qReq = C.qlib_request(key: key, message: msg, queueName: queue)
     add_record(table, qReq)
@@ -69,6 +109,23 @@ defmodule QlibDB do
 	{:atomic, :ok}
       {:aborted, e} ->
 	raise(e, [{table, options}])
+    end
+  end
+
+  @doc """
+  Checks whether the key provided is the first, then deletes it.
+  """
+  @spec remove_all(:mnesia.tab(), String.t()) :: :ok | no_return()
+  def remove_all(table, queue) do
+    case :mnesia.dirty_select(table, [{{'$1','$2','$3','$4'},
+				       [{'=:=','$4',{:const, queue}}],
+				       [{{'$2'}}]}]) do
+      keys = [_|_] ->
+	for {key} <- keys, do: delete_chk_q(table, key)
+	:ok
+      [] -> :ok
+      first ->
+	raise({:bad_delete, table, first})
     end
   end
   
